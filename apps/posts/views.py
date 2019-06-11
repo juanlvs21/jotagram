632fb00a6b8a50a2524566ef7fe81ff67f00590a
Juan Villarroel <=>
2019-06-11 12:39:43 -0400

    removing unsed assets

--- apps/posts/views.py
+++ apps/posts/views.py
@@ -9,45 +9,16 @@ from datetime import datetime
 # Models
 from django.contrib.auth.models import User
 from apps.users.models import Profile
+from apps.posts.models import Post
 
 # Form
 from apps.posts.forms import PostForm
 
-posts = [
-    {
-        'title':'Mont Blac',
-        'user':{
-            'name':'Yésica Cortés',
-            'picture':'https://picsum.photos/60/60/?image=1027',
-        },
-        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'photo':'https://picsum.photos/500/500/?image=1036',
-    },
-    {
-        'title':'Via Láctea',
-        'user':{
-            'name':'Christian Van der Henst',
-            'picture':'https://picsum.photos/60/60/?image=1005',
-        },
-        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'photo':'https://picsum.photos/500/500/?image=903',
-    },
-    {
-        'title':'Nuevo auditorio',
-        'user':{
-            'name':'Uriel (Thespianartist)',
-            'picture':'https://picsum.photos/60/60/?image=883',
-        },
-        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'photo':'https://picsum.photos/500/500/?image=1076',
-    },
-]
-
 @login_required
 def list_post(request):
     """List existing posts."""
-    # return render(request, 'posts/posts.html', {'posts':posts})
     profile = request.user.profile
+    posts = Post.objects.all().order_by('-created')
     return render(
         request=request,
         template_name='posts/posts.html',
